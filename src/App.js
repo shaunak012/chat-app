d72d706295f482f592c76c0e009c9260591f9a30
shaunak012 <shaunak023@gmail.com>
2023-05-29 17:41:34 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [messages, setMessages] = useState([{
+    sender: "Me",
+    content: "HI MUMMY"
+  }]);
+
+  // Function to handle sending a message
+  const sendMessage = (e) => {
+    e.preventDefault();
+    // Logic to send the message to the server or update the message state
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="chat-app">
+        <div className="chat-header">
+          <h2>Global Chat Room</h2>
+        </div>
+
+        <div className="chat-messages">
+          {/* Render the messages from the state */}
+          {messages.map((message, index) => (
+            <div key={index} className="message">
+              <span className="sender">{message.sender}: </span>
+              <span className="content">{message.content}</span>
+            </div>
+          ))}
+        </div>
+
+        <form className="chat-form" onSubmit={sendMessage}>
+          <input
+            type="text"
+            placeholder="Type your message..."
+          // Update the input value using state and onChange event
+          />
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </>
   );
 }
 
