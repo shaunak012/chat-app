9ee341baaac355ce2ab664594d0eb57727b0f74a
shaunak012 <shaunak023@gmail.com>
2023-06-04 12:57:17 +0530

    Added private rooms

--- nodeServer/server.js
+++ nodeServer/server.js
@@ -10,31 +10,49 @@ app.use(cors);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000" || "https://sensational-lolly-f84033.netlify.app/"
+        origin: process.env.PORT || "http://localhost:3000"
     }
 });
 
 const users = {}
-
+const others = []
 
 io.on('connection', (socket) => {
 
     socket.on("new-user-joined", name => {
+        socket.join("global")
+        others.push("global")
         users[socket.id] = name;
-        socket.broadcast.emit(`user-joined`, users[socket.id]);
-        console.log(name, " : ", socket.id);
+        socket.broadcast.to("global").emit(`user-joined`, users[socket.id]);
+        console.log(name, " : connected");
     })
 
-    socket.on(`send`, message => {
+    socket.on(`send`, data => {
         // socket.broadcast
-        io.emit("recieve", {
-            message: message,
+        io.to(data.currentChat).emit("recieve", {
+            message: data.message,
             name: users[socket.id]
         })
     })
+    socket.on("join-old-chat", chatName => {
+        others.map((chats) => {
+            socket.leave(chats)
+        })
+        socket.join(chatName)
+        socket.broadcast.to(chatName).emit(`user-joined`, users[socket.id]);
+    })
+    socket.on("join-new-group", (name) => {
+        socket.broadcast.to(name).emit(`user-joined`, users[socket.id]);
+        others.push(name)
+        others.map((chats) => {
+            socket.leave(chats)
+        })
+        socket.join(name);
+        socket.emit("clear")
+    })
     socket.on('disconnect', () => {
         socket.broadcast.emit("user-disconnected", users[socket.id])
-        console.log('user disconnected');
+        console.log(users[socket.id], ': disconnected');
         delete users[socket.id]
     });
 });
--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import io from "socket.io-client"
 
 let socket
 const username = prompt('What is your name?');
+const PORT = 'http://localhost:5000'
 function App() {
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState('');
-
+  const [chats, setChats] = useState([]);
+  const [currentChat, setCurrentChat] = useState("global")
   useEffect(() => {
 
 
-    socket = io.connect('http://localhost:5000' || "https://sensational-lolly-f84033.netlify.app/server");
+    socket = io.connect(PORT);
 
     socket.emit('new-user-joined', username);
 
@@ -25,6 +27,9 @@ function App() {
     });
     socket.on('user-disconnected', (name) => {
       append(name, ' left the chat');
+    });
+    socket.on("clear", () => {
+      setMessages([]);
     });
     return () => {
     }
@@ -49,26 +54,62 @@ function App() {
       setMessage('');
 
       // Send the message to the server
-      socket.emit("send", message);
+      socket.emit("send", { message, currentChat });
     }
   };
+  const AddGroup = () => {
+    const name = prompt("Room Id");
+    if (name !== null && name.trim() !== "") {
+      setChats((prevChat) => [
+        ...prevChat,
+        {
+          name: name
+        },
+      ]);
+      socket.emit("join-new-group", name)
+      setCurrentChat(name)
+    }
+  }
+  const joinChat = (chatName) => {
 
-
+    socket.emit("join-old-chat", chatName)
+    setMessages([]);
+    setCurrentChat(chatName);
+  }
   return (
     <>
       <div className="chat-app">
         <div className="chat-header">
           <h2>Global Chat Room</h2>
         </div>
+        <div className='messaging-section'>
 
-        <div className="chat-messages">
-          {/* Render the messages from the state */}
-          {messages.map((message, index) => (
-            <div key={index} className={`message`}>
-              <span className="sender">{message.sender}: </span>
-              <span className="content">{message.content}</span>
-            </div>
-          ))}
+          <div className='Sidebar'>
+            <button id='Global' className='Chat' onClick={() => {
+              joinChat("global")
+            }}>Global</button>
+
+            {chats.map((chat, index) => (
+              <button key={index} className='Chat' onClick={() => {
+                joinChat(chat.name)
+              }}>{chat.name}</button>
+            ))}
+
+            <button id='Add-Group' className='Chat' onClick={() => {
+              AddGroup()
+            }}>Add Room</button>
+          </div>
+
+
+          <div className="chat-messages">
+            {/* Render the messages from the state */}
+            {messages.map((message, index) => (
+              <div key={index} className={`message`}>
+                <span className="sender">{message.sender}: </span>
+                <span className="content">{message.content}</span>
+              </div>
+            ))}
+          </div>
         </div>
 
         <form className="chat-form" onSubmit={sendMessage}>
@@ -82,7 +123,7 @@ function App() {
           />
           <button type="submit" >Send</button>
         </form>
-      </div>
+      </div >
     </>
   );
 }
